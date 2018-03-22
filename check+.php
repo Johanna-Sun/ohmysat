96acc7b6c493bc4c84ea28746a68f0cbeb070427
JohannaSun <sunyx.johanna2016@outlook.com>
2018-03-22 17:13:20 +0800

    cb html change

--- check+.php
+++ check+.php
@@ -1,4 +1,5 @@
 <?php
+error_reporting( E_ALL&~E_NOTICE );
 
 include_once('simple_html_dom.php');
 
@@ -13,11 +14,15 @@ while(true)
 	curl_setopt($c, CURLOPT_HTTPHEADER, array($cookie));
 	$data=curl_exec($c);
 	// echo $data;
-	$html=str_get_html($data);
-	$ret=$html->find('.nsat-home-testScoreDate');
-	// echo $ret[0];
+	$html = new simple_html_dom();
+	// $html=str_get_html($data);
+	$html->load($data);
+	// var_dump($html);
+	$ret=$html->find('div.nsat-home-test-info h3');
+	// echo $ret[0]->plaintext;
+	// echo 'SAT with Essay &mdash; '.$argv[1];
 	echo "还没有 别急\n";
-	if($ret[0] != '<div class="nsat-home-testScoreDate">'.$argv[1].'</div>')
+	if($ret[0]->plaintext != 'SAT with Essay &mdash; '.$argv[1])
 	{
 		echo 'HOLY SHIT!!!';
 		for($i=0;$i<100;$i++){
